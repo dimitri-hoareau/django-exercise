bbb313955b2f09e4239bb3cd8c3e34017818fdd5
dimitri-hoareau <dimitrihoareau2@gmail.com>
2023-04-10 18:00:54 +0200

    add pagination, total_sellingprice and category field into sale list

--- main/settings/rest.py
+++ main/settings/rest.py
@@ -1,6 +1,8 @@
 from main.jsonenv import env
 
 REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS":"rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 25,
     "DEFAULT_AUTHENTICATION_CLASSES": (
         # 'rest_framework.authentication.SessionAuthentication',
         # "rest_framework.authentication.TokenAuthentication",
--- sales/serializers.py
+++ sales/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework.serializers import ModelSerializer
+
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
  
 from sales.models import Article, Sale
  
@@ -11,12 +12,28 @@ class ArticleSerializer(ModelSerializer):
         model = Article
         fields = ['id', 'code', 'category', 'name', 'manufacturing_cost']
 
+class ArticleBasicSerializer(ModelSerializer):
+    """
+    Serializer for the Article model, including only the category and name fields,
+    used into SaleSerializer
+    """
+
+    class Meta:
+        model = Article
+        fields = ['category', 'name']
+
 
 class SaleSerializer(ModelSerializer):
     """
     Serializer for the Sale model
     """
+
+    article = ArticleBasicSerializer()
+    total_selling_price = SerializerMethodField()
  
     class Meta:
         model = Sale
-        fields = ['date', 'author', 'article', 'quantity', 'unit_selling_price']
+        fields = ['id','date', 'author', 'article', 'quantity', 'unit_selling_price', 'total_selling_price']
+
+    def get_total_selling_price(self, obj):
+        return obj.quantity * obj.unit_selling_price
